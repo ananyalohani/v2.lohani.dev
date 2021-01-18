f48bc1f0ce4c4e5f0929926b41b367a7588c9101
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-01-18 16:10:32 +0530

    Add About section and Heading component

---
+++ src/components/About.svelte
@@ -0,0 +1,53 @@
+<script>
+  import Heading from './Heading.svelte';
+  let headingText = "About";
+</script>
+
+<div id="about">
+  <Heading {headingText} />
+  <div class="section-content">
+    <img class="my-photo" src="./images/my-photo.png" alt="Ananya Lohani"/>
+    <p class="about-me">Hello, I am Ananya. Welcome to my website!
+      <br/>
+      <br/>
+      I am a Sophomore at <a href="https://iiitd.ac.in/" target="_blank">IIIT Delhi</a>, majoring in Computer Science and Engineering. My recent interests include web development and UI/UX design. I am currently a Web Design intern at <a href="https://vincistationery.com" target="_blank">Vinci Stationery</a>.
+      <br/>
+      <br/>
+      I paint, sometimes. You can see all my artwork on my <a href="https://www.instagram.com/lohani_art/" target="_blank">art account</a>.</p>
+  </div>
+</div>
+
+<style>
+  #about {
+    color: #bae4eb;
+    margin-top: 40px;
+    font-family: 'Overpass Mono', monospace;
+    font-weight: 300;
+  }
+
+  a:hover {
+    color:#8db1b8;
+  }
+
+  .section-content {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    vertical-align: middle;
+  }
+
+  .about-me {
+    padding-top: 50px;
+  }
+
+  .my-photo {
+    height: 450px;
+    width: 450px;
+  }
+  
+  p {
+    vertical-align: middle;
+  }
+
+</style>
---
+++ src/components/Heading.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let headingText;
+</script>
+
+<div class="section-heading">
+  <div class="wrapper">
+    <h1>{headingText}</h1>
+    <div class="background-deco"></div>
+  </div>
+</div>
+
+<style>
+  .section-heading {
+    padding: 0;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    vertical-align: center;
+  }
+
+  .wrapper {
+    text-align: center;
+    font-family: 'Overpass', sans-serif;
+    color: white;
+    width: min-content;
+  }
+
+  h1 {
+    position: relative;
+    font-weight: 500;
+    letter-spacing: 3px;
+    text-shadow: 0px 4px 10px rgba(0, 0, 0, 0.5);
+    font-size: 35px;
+  }
+
+  .background-deco {
+    height: 10px;
+    background-color: rgb(11, 105, 109);
+    margin-top: -45px;
+  }
+</style>
